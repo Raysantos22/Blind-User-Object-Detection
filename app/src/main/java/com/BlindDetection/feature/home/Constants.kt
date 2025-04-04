d3519a1f63f95be9a4c989a04ce57c0ca9205143
PROGRAMMER <qqqraysantos@gmail.com>
2025-04-04 17:50:15 +0800

    99 percent

--- app/src/main/java/com/BlindDetection/feature/home/Constants.kt
+++ app/src/main/java/com/BlindDetection/feature/home/Constants.kt
@@ -1,6 +1,6 @@
 package com.BlindDetection.feature.home
 
 object Constants {
-    const val MODEL_PATH = "model.tflite"
+    const val MODEL_PATH = "yolov9.tflite"
     const val LABELS_PATH = "labels1.txt"
 }
